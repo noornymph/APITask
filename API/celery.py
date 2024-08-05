10f04524f7a248202089b66e638f7d1e0da9bce3
Khadija <khadijaaranoor2002@gmail.com>
2024-08-05 14:38:00 +0500

    celery.py created

---
+++ API/celery.py
@@ -0,0 +1,25 @@
+"""
+This module contains celery code.
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+import os
+
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+
+app = Celery("API")
+
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    """
+    This task is used to output the details of the Celery task request.
+    """
+    print(f"Request: {self.request!r}")
